0445ff344f6104c5ef1e3d344b26aa4697e74c8c
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-05-11 23:55:50 -0300

    Update System2.0.lua

--- System2.0.lua
+++ System2.0.lua
@@ -8,6 +8,14 @@ EquipUseHoldDelay("Nome do item", "Skill", tempo segurando, Delay para próximo 
 exemplo:
 EquipUseHoldDelay("Godhuman", "C", 0.5, 1.7)
 
+EquipItem("nome do item")
+exemplo:
+EquipItem("Godhuman")
+
+UseSkill("Skill")
+exemplo:
+UseSkill("X")
+
 raca()
 ativa a habilidade da raca v3
 
@@ -80,3 +88,32 @@ function raca()
  }
  game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("CommE"):FireServer(unpack(args))
 end 
+
+--- Funções 
+
+local function EquipItem(Equipe)
+    local humanoid = Player.Character and Player.Character:FindFirstChildOfClass("Humanoid")
+    if humanoid then
+        humanoid:UnequipTools()
+        task.wait(0.1)
+        local tool = Player.Backpack:FindFirstChild(Equipe)
+        if tool then
+            humanoid:EquipTool(tool)
+            local start = tick()
+            repeat
+                task.wait()
+            until (Player.Character:FindFirstChildOfClass("Tool") and Player.Character:FindFirstChildOfClass("Tool").Name == Equipe) or tick() - start > 2
+        else
+            warn(Equipe .. " não encontrada no inventário.")
+        end
+    end
+end
+
+--- Funções 
+
+local function UseSkill(skill)
+    local VirtualInput = game:GetService("VirtualInputManager")
+    VirtualInput:SendKeyEvent(true, skill, false, game)
+    task.wait(0.02)
+    VirtualInput:SendKeyEvent(false, skill, false, game)
+end
