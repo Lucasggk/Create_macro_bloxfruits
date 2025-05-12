e2c4a23cfb73ecf9254eb1065c80602e4a592ce1
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-05-12 01:24:22 -0300

    Update Ui.loader.lua

--- Ui.loader.lua
+++ Ui.loader.lua
@@ -1,3 +1,21 @@
 local name = getgenv().uiname
 local script = getgenv().script
 
+local ScreenGui = Instance.new("ScreenGui", game.CoreGui)
+local Button = Instance.new("TextButton", ScreenGui)
+
+Button.Size = UDim2.new(0, 120, 0, 40)
+Button.Position = UDim2.new(0.5, -60, 0.8, 0)
+Button.Text = name
+Button.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
+Button.TextColor3 = Color3.fromRGB(255, 255, 255)
+Button.Font = Enum.Font.SourceSansBold
+Button.TextSize = 20
+Button.BorderSizePixel = 0
+Button.BackgroundTransparency = 0.2
+Button.Active = true
+Button.Draggable = true
+
+Button.MouseButton1Click:Connect(function()
+    loadstring(script)()
+  end)
